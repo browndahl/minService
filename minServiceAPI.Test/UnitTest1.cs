d12b8ba17c06386f044c8e411e3156b2c3931aaf
“browndahl” <mrbrowndahl@gmail.com>
2023-09-28 10:43:16 +0200

    first commit

---
+++ minServiceAPI.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace minServiceAPI.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
